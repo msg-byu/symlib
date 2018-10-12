0e440b2851dce85d7739eea8306679b3336269e5
Wiley Morgan <wiley.s.morgan@gmail.com>
2018-10-12 12:25:47 -0600

    Removed unused variable in is_eqiv_lattice.

--- src/compare_structures.f90
+++ src/compare_structures.f90
@@ -232,8 +232,8 @@ CONTAINS
     real(dp), intent(in) :: lat1(3,3), lat2(3,3), eps
     logical :: is_equiv_lattice, err
     real(dp)  :: lat1inv(3,3), S(3,3)
-    integer i
     real(dp) :: atol  ! An absolute tolerance for the "equal" function
+
     atol = 1E-6_dp
     is_equiv_lattice = .false.
     call matrix_inverse(lat1,lat1inv,err)
